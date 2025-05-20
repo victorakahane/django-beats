6cfab8a2280cbc308c069295b9daae2a136f9c6e
VictorAkahaneSantos <victor.santos@debiasi.com.br>
2025-05-20 12:27:33 -0300

    Feat: add models to admin page

--- django_beats/festival/admin.py
+++ django_beats/festival/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import FestivalDay, Artist
 
-# Register your models here.
+class ArtistInline(admin.TabularInline):
+    """
+    Inline admin interface for the Artist model.
+    """
+    model = Artist
+    extra = 1 # Number of empty forms to display
+
+@admin.register(FestivalDay)
+class FestivalDayAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the FestivalDay model.
+    """
+    inlines = [ArtistInline] # Show artists inline in the FestivalDay admin
+    list_display = ('date',)
+
+@admin.register(Artist)
+class ArtistAdmin(admin.ModelAdmin):
+    """
+    Admin interface for the Artist model.
+    """
+    list_display = ('name', 'performance_time', 'day') # Show name, performance time, and day in the admin
+    list_filter = ('day',) # Side filter for days
+    ordering = ('day', 'performance_time')
